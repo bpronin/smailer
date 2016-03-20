3cb50aa1a0764577d1f69b97403b3876148f266e
Boris Pronin <boris.i.pronin@gmail.com>
2016-03-20 16:24:59 +0300

    fixed log capacity issue. fixes #52

--- app/src/androidTest/java/com/bopr/android/smailer/ActivityLogTest.java
+++ app/src/androidTest/java/com/bopr/android/smailer/ActivityLogTest.java
@@ -1,11 +1,13 @@
 package com.bopr.android.smailer;
 
 import android.app.Application;
+import android.support.annotation.NonNull;
 import android.test.ApplicationTestCase;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
+import java.util.concurrent.TimeUnit;
 
 /**
  * {@link ActivityLog} tester.
@@ -19,14 +21,53 @@ public class ActivityLogTest extends ApplicationTestCase<Application> {
         Locale.setDefault(Locale.US);
     }
 
+    @NonNull
+    private List<ActivityLogItem> asList(ActivityLog log) {
+        List<ActivityLogItem> items = new ArrayList<>();
+
+        ActivityLog.Cursor cursor = log.getAll();
+        cursor.moveToFirst();
+        while (!cursor.isAfterLast()) {
+            items.add(cursor.get());
+            cursor.moveToNext();
+        }
+        cursor.close();
+
+        return items;
+    }
+
     @Override
     public void setUp() throws Exception {
         super.setUp();
-        log = ActivityLog.getInstance(getContext());
-        log.clear();
+        log = new ActivityLog(getContext());
+        getContext().deleteDatabase(ActivityLog.DB_NAME);
     }
 
-    public void testAll() throws Exception {
+    public void testDefaults() throws Exception {
+        assertEquals(10000, log.getCapacity());
+        assertEquals(TimeUnit.DAYS.toMillis(1), log.getPurgePeriod());
+    }
+
+    public void testClear() throws Exception {
+        log.success(new MailMessage("+79052345671", true, 1000, 0, false, true, null, null));
+        log.success(new MailMessage("+79052345672", false, 2000, 0, false, true, null, null));
+        log.success(new MailMessage("+79052345673", true, 3000, 0, false, false, null, null));
+        log.success(new MailMessage("+79052345674", false, 4000, 0, false, false, null, null));
+        log.success(new MailMessage("+79052345675", true, 5000, 0, true, false, null, null));
+        log.error(new MailMessage("+79052345671", true, 6000, 0, false, true, null, null), new Exception("Test 1"));
+        log.error(new MailMessage("+79052345672", false, 7000, 0, false, true, null, null), new Exception("Test 2"));
+        log.error(new MailMessage("+79052345673", true, 8000, 0, false, false, null, null), new Exception("Test 3"));
+        log.error(new MailMessage("+79052345674", false, 9000, 0, false, false, null, null), new Exception("Test 4"));
+        log.error(new MailMessage("+79052345675", true, 10000, 0, true, false, null, null), new Exception("Test 5"));
+
+        assertEquals(10, log.getAll().getCount());
+
+        log.clear();
+
+        assertEquals(0, log.getAll().getCount());
+    }
+
+    public void testAddGet() throws Exception {
         log.success(new MailMessage("+79052345671", true, 1000, 0, false, true, null, null));
         log.success(new MailMessage("+79052345672", false, 2000, 0, false, true, null, null));
         log.success(new MailMessage("+79052345673", true, 3000, 0, false, false, null, null));
@@ -39,14 +80,7 @@ public class ActivityLogTest extends ApplicationTestCase<Application> {
         log.error(new MailMessage("+79052345674", false, 9000, 0, false, false, null, null), new Exception("Test 4"));
         log.error(new MailMessage("+79052345675", true, 10000, 0, true, false, null, null), new Exception("Test 5"));
 
-        List<ActivityLogItem> items = new ArrayList<>();
-
-        ActivityLog.Cursor cursor = log.getAll();
-        cursor.moveToFirst();
-        while (!cursor.isAfterLast()) {
-            items.add(cursor.get());
-            cursor.moveToNext();
-        }
+        List<ActivityLogItem> items = asList(log);
 
         assertEquals(10, items.size());
 
@@ -68,6 +102,48 @@ public class ActivityLogTest extends ApplicationTestCase<Application> {
         assertEquals("java.lang.Exception: Test 3", items.get(2).getDetails());
         assertEquals("java.lang.Exception: Test 2", items.get(3).getDetails());
         assertEquals("java.lang.Exception: Test 1", items.get(4).getDetails());
+    }
+
+    public void testPurge() throws Exception {
+        log.success(new MailMessage("1", true, 1000, 0, false, true, null, null));
+        log.success(new MailMessage("2", false, 2000, 0, false, true, null, null));
+        log.success(new MailMessage("3", true, 3000, 0, false, false, null, null));
+        log.success(new MailMessage("4", false, 4000, 0, false, false, null, null));
+        log.success(new MailMessage("5", true, 5000, 0, true, false, null, null));
+        log.error(new MailMessage("6", true, 6000, 0, false, true, null, null), new Exception("Test 1"));
+        log.error(new MailMessage("7", false, 7000, 0, false, true, null, null), new Exception("Test 2"));
+        log.error(new MailMessage("8", true, 8000, 0, false, false, null, null), new Exception("Test 3"));
+        log.error(new MailMessage("9", false, 9000, 0, false, false, null, null), new Exception("Test 4"));
+
+        /* first we have 9 records */
+        assertEquals(9, log.getAll().getCount());
+
+        /* change default capacity and period to small values */
+        log.setCapacity(5);
+        log.setPurgePeriod(100);
+
+        /* sleep a time that is less than purge period */
+        Thread.sleep(10);
+        /* and add another record */
+        log.error(new MailMessage("10", true, 10000, 0, true, false, null, null), new Exception("Test 5"));
+
+        /* nothing happen cause elapsed time is less than purge period */
+        assertEquals(10, log.getAll().getCount());
+
+        /* sleep again */
+        Thread.sleep(90);
+        /* and add another record */
+        log.success(new MailMessage("11", true, 1000, 0, false, true, null, null));
+
+        /* last addition should trigger purge process cause total elapsed time exceeds purge period */
+        assertEquals(5, log.getAll().getCount());
+
+        List<ActivityLogItem> items = asList(log);
+        assertEquals("Incoming SMS from 11. Send success.", items.get(0).getMessage());
+        assertEquals("Missed call from 10. Send failed.", items.get(1).getMessage());
+        assertEquals("Outgoing call to 9. Send failed.", items.get(2).getMessage());
+        assertEquals("Incoming call from 8. Send failed.", items.get(3).getMessage());
+        assertEquals("Outgoing SMS to 7. Send failed.", items.get(4).getMessage());
     }
 
 }
--- app/src/main/java/com/bopr/android/smailer/ActivityLog.java
+++ app/src/main/java/com/bopr/android/smailer/ActivityLog.java
@@ -23,21 +23,23 @@ public class ActivityLog {
 
     public static final int LEVEL_ERROR = Log.ERROR;
     public static final int LEVEL_INFO = Log.INFO;
-    public static final int DEFAULT_CAPACITY = 10000;
     private static final int DB_VERSION = 1;
     public static final String DB_NAME = "smailer.sqlite";
     private static final String TABLE_LOG = "activity_log";
+    private static final String TABLE_SYSTEM = "system_data";
     private static final String COLUMN_ID = "_id";
     private static final String COLUMN_LOG_TIMESTAMP = "timestamp";
     private static final String COLUMN_LOG_MESSAGE = "message";
     private static final String COLUMN_LOG_LEVEL = "level";
     private static final String COLUMN_LOG_DETAILS = "details";
+    private static final String COLUMN_PURGE_TIME = "purge_time";
 
     public static ActivityLog instance;
 
     private final Context context;
     private final DbHelper helper;
-    private int capacity = DEFAULT_CAPACITY;
+    private int capacity = 10000;
+    private long purgePeriod = 86400000;
 
     public static ActivityLog getInstance(Context context) {
         if (instance == null) {
@@ -50,15 +52,13 @@ public class ActivityLog {
         return instance;
     }
 
-    private ActivityLog(Context context) {
+    protected ActivityLog(Context context) {
         this.context = context;
         helper = new DbHelper(context);
     }
 
     /**
-     * Returns current maximum amount of records that the log may contain.
-     *
-     * @return maximum amount of records
+     * Returns maximum amount of records that the log may contain.
      */
     public int getCapacity() {
         return capacity;
@@ -72,7 +72,22 @@ public class ActivityLog {
      */
     public void setCapacity(int capacity) {
         this.capacity = capacity;
-        purge();
+    }
+
+    /**
+     * Returns time period that should exceed until addition of new record triggers purge process
+     * that removes stale records.
+     */
+    public long getPurgePeriod() {
+        return purgePeriod;
+    }
+
+    /**
+     * Sets time period that should exceed until addition of new record triggers purge process
+     * that removes stale records.
+     */
+    public void setPurgePeriod(long purgePeriod) {
+        this.purgePeriod = purgePeriod;
     }
 
     /**
@@ -80,7 +95,7 @@ public class ActivityLog {
      *
      * @param message log message
      */
-    public void success(MailMessage message) {
+    public synchronized void success(MailMessage message) {
         ActivityLogItem item = new ActivityLogItem(ActivityLog.LEVEL_INFO);
         item.setMessage(formatMessage(message, R.string.activity_log_message_send_email_success));
         add(item);
@@ -91,7 +106,7 @@ public class ActivityLog {
      *
      * @param message log message
      */
-    public void error(MailMessage message, Exception error) {
+    public synchronized void error(MailMessage message, Exception error) {
         ActivityLogItem item = new ActivityLogItem(ActivityLog.LEVEL_ERROR);
         item.setMessage(formatMessage(message, R.string.activity_log_message_send_email_failed));
         item.setDetails(error.toString());
@@ -103,7 +118,7 @@ public class ActivityLog {
      *
      * @return cursor for all records
      */
-    public Cursor getAll() {
+    public synchronized Cursor getAll() {
         return new Cursor(helper.getReadableDatabase().query(TABLE_LOG,
                 null,
                 null,
@@ -115,24 +130,75 @@ public class ActivityLog {
     }
 
     /**
-     * Removes all older records that exceeds specified capacity.
+     * Removes all records from log.
      */
-    public void purge() {
-//        String sql = "delete from " + TABLE_LOG +
-//                " where " + COLUMN_ID + " not in " +
-//                "(" +
-//                "select " + COLUMN_ID + " from " + TABLE_LOG +
-//                " order by " + COLUMN_ID + " desc " +
-//                "limit " + capacity +
-//                ")";
-//        helper.getWritableDatabase().execSQL(sql);
+    public synchronized void clear() {
+        SQLiteDatabase db = helper.getWritableDatabase();
+        db.beginTransaction();
+        try {
+            db.delete(TABLE_LOG, null, null);
+            updateLastPurgeTime(db);
+
+            db.setTransactionSuccessful();
+        } finally {
+            db.endTransaction();
+        }
     }
 
     /**
-     * Removes all records from log.
+     * Removes all stale records that exceeds specified capacity if given
+     * period of time has elapsed.
      */
-    public void clear() {
-        helper.getWritableDatabase().delete(TABLE_LOG, null, null);
+    private void purge() {
+        SQLiteDatabase db = helper.getWritableDatabase();
+        db.beginTransaction();
+        try {
+            if (getCurrentSize(db) >= capacity
+                    && System.currentTimeMillis() - getLastPurgeTime(db) >= purgePeriod) {
+                db.execSQL("delete from " + TABLE_LOG +
+                        " where " + COLUMN_ID + " not in " +
+                        "(" +
+                        "select " + COLUMN_ID + " from " + TABLE_LOG +
+                        " order by " + COLUMN_ID + " desc " +
+                        "limit " + capacity +
+                        ")");
+
+                updateLastPurgeTime(db);
+            }
+            db.setTransactionSuccessful();
+        } finally {
+            db.endTransaction();
+        }
+    }
+
+    private long getCurrentSize(SQLiteDatabase db) {
+        android.database.Cursor cursor = db.query(TABLE_LOG,
+                new String[]{"COUNT(*)"}, null, null, null, null, null);
+        cursor.moveToFirst();
+        try {
+            return cursor.getLong(0);
+        } finally {
+            cursor.close();
+        }
+    }
+
+    private long getLastPurgeTime(SQLiteDatabase db) {
+        android.database.Cursor cursor = db.query(TABLE_SYSTEM,
+                new String[]{COLUMN_PURGE_TIME}, COLUMN_ID + "=0", null, null, null, null);
+        cursor.moveToFirst();
+        try {
+            return cursor.getLong(cursor.getColumnIndex(COLUMN_PURGE_TIME));
+        } finally {
+            cursor.close();
+        }
+    }
+
+    private void updateLastPurgeTime(SQLiteDatabase db) {
+        ContentValues values = new ContentValues();
+        values.put(COLUMN_ID, 0);
+        long value = System.currentTimeMillis();
+        values.put(COLUMN_PURGE_TIME, value);
+        db.replace(TABLE_SYSTEM, null, values);
     }
 
     private void add(ActivityLogItem item) {
@@ -144,6 +210,8 @@ public class ActivityLog {
 
         long id = helper.getWritableDatabase().insert(TABLE_LOG, null, values);
         item.setId(id);
+
+        purge();
     }
 
     @NonNull
@@ -188,6 +256,11 @@ public class ActivityLog {
                     COLUMN_LOG_MESSAGE + " varchar(100)," +
                     COLUMN_LOG_DETAILS + " varchar(200)" +
                     ")");
+            db.execSQL("create table " + TABLE_SYSTEM + " (" +
+                    COLUMN_ID + " integer primary key, " +
+                    COLUMN_PURGE_TIME + " integer" +
+                    ")");
+            updateLastPurgeTime(db);
         }
 
         @Override
