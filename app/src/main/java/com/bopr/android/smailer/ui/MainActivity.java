07afb1b725622d3d08d42d1ceafc7ca4b8294232
bpronin <boris.i.pronin@gmail.com>
2019-08-01 11:01:55 +0300

    synchronization

--- app/src/main/java/com/bopr/android/smailer/ui/MainActivity.java
+++ app/src/main/java/com/bopr/android/smailer/ui/MainActivity.java
@@ -52,7 +52,7 @@ public class MainActivity extends AppActivity {
     }
 
     @Override
-    protected void onDestroy() {
+    public void onDestroy() {
         syncManager.dispose();
         super.onDestroy();
     }
