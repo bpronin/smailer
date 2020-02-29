36d757110718f4bac5287f21577a6f16bab3f7d5
bpronin <boris.i.pronin@gmail.com>
2020-02-29 13:44:24 +0300

    refactoring

--- app/src/free/kotlin/com/bopr/android/smailer/ui/BaseAppActivity.kt
+++ app/src/free/kotlin/com/bopr/android/smailer/ui/BaseAppActivity.kt
@@ -1,9 +1,11 @@
 package com.bopr.android.smailer.ui
 
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import com.bopr.android.smailer.R
 import com.google.android.gms.ads.AdRequest
 import com.google.android.gms.ads.AdView
+import kotlin.reflect.KClass
 
 /**
  * Base application activity. Individual in different build variants.
@@ -12,7 +14,7 @@ import com.google.android.gms.ads.AdView
  *
  * @author Boris Pronin ([boprsoft.dev@gmail.com](mailto:boprsoft.dev@gmail.com))
  */
-abstract class BaseAppActivity : BaseActivity() {
+abstract class BaseAppActivity(fragmentClass: KClass<out Fragment>) : BaseActivity(fragmentClass) {
 
     private lateinit var adView: AdView
 
