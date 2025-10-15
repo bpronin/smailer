f363a7dd3001057a359a55147d77d2ca6828a8fa
bpronin <boris.i.pronin@gmail.com>
2025-10-15 23:05:35 +0300

    update gradle
    Signed-off-by: bpronin <boris.i.pronin@gmail.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:8.10.1")
+        classpath("com.android.tools.build:gradle:8.13.0")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.0.21")
         classpath("org.jetbrains.kotlin:kotlin-allopen:2.0.21")
         classpath("com.google.gms:google-services:4.4.2")
