c7c6229ae5684fca631081e57ee84c7c41c8870e
bpronin <boris.i.pronin@gmail.com>
2026-03-05 14:28:02 +0300

    refactoring
    Signed-off-by: bpronin <boris.i.pronin@gmail.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,11 +6,11 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:9.1.0")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.2.10")
-        classpath("org.jetbrains.kotlin:kotlin-allopen:2.2.10")
-        classpath("com.google.gms:google-services:4.4.4")
-        classpath("com.google.firebase:firebase-crashlytics-gradle:3.0.6")
+        classpath(libs.gradle)
+        classpath(libs.kotlin.gradle.plugin)
+        classpath(libs.kotlin.allopen)
+        classpath(libs.google.services)
+        classpath(libs.firebase.crashlytics.gradle)
     }
 }
 
